b4e14d47d0b531d1378f1958b724f1db25a8eb38
Sayak Mohanta <sayakmohanta1998@gmail.com>
2022-07-04 23:56:18 +0530

    Updated

--- react-weather/src/App.js
+++ react-weather/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import UilReact from "@iconscout/react-unicons/icons/uil-react";
+import TopButtons from "./components/TopButtons";
+import { Box } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className="App">
+      <TopButtons />
+    </Box>
   );
 }
 
 export default App;
+
+//11:50
