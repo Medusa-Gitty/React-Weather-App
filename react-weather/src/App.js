2865d9956f83618a33967d91ef448be9f5f8bf2e
Sayak Mohanta <sayakmohanta1998@gmail.com>
2022-07-07 19:07:16 +0530

    API

--- react-weather/src/App.js
+++ react-weather/src/App.js
@@ -6,8 +6,18 @@ import Inputs from "./components/Inputs";
 import TimeAndLocation from "./components/TimeAndLocation";
 import TempAndDetails from "./components/TempAndDetails";
 import Forecast from "./components/Forecast";
+import { useEffect } from "react";
+import getFormattedWeatherData from "./services/weatherService";
 
 function App() {
+  useEffect(() => {
+    async function weatherData() {
+      const data = await getFormattedWeatherData({ q: "asansol" });
+      console.log(data);
+    }
+    weatherData();
+  }, []);
+
   return (
     <VStack p={4} gap={3}>
       <TopButtons />
@@ -24,3 +34,7 @@ function App() {
 export default App;
 
 //11:50
+//005feeb72e0838e3972590465735efbb
+
+//City name:
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
