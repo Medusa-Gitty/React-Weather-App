d34bf96b3ff747e83c808a5300e531fb59fbc49b
Sayak Mohanta <sayakmohanta1998@gmail.com>
2022-07-08 13:02:19 +0530

    UI

--- react-weather/src/App.js
+++ react-weather/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Spinner, VStack } from "@chakra-ui/react";
+import { Spinner, VStack, Image } from "@chakra-ui/react";
 import TopButtons from "./components/TopButtons";
 import Inputs from "./components/Inputs";
 import TimeAndLocation from "./components/TimeAndLocation";
@@ -9,11 +9,22 @@ import { useEffect } from "react";
 import getFormattedWeatherData from "./services/weatherService";
 import { useSelector, useDispatch } from "react-redux";
 import { weatherQueryActions } from "./store/weather-query-slice";
+import cool from "./assets/cool4.jpg";
+import hot from "./assets/hot3.jpg";
 
 function App() {
   const dispatch = useDispatch();
   let { queries, units, weather } = useSelector((state) => state.weather);
 
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
+        dispatch(weatherQueryActions.geoLocation({ lat, lon }));
+      });
+    }
+  }, [dispatch]);
   //Fetching Weather
   useEffect(() => {
     async function weatherData() {
@@ -24,38 +35,31 @@ function App() {
   }, [queries, units, dispatch]);
 
   return (
-    <VStack p={4} gap={3}>
-      <TopButtons />
-      <Inputs />
-      {weather && (
-        <>
-          <TimeAndLocation />
-          <TempAndDetails />
-          <Forecast title="hourly" />
-          <Forecast title="daily" />
-        </>
-      )}
-      {!weather && (
-        <Spinner
-          thickness="4px"
-          speed="0.65s"
-          emptyColor="gray.200"
-          color="blue.500"
-          size="xl"
-        />
-      )}
-    </VStack>
+    <>
+      <Image src={cool} className="image" />
+      <VStack p={5} gap={3} className="texts" w={["100%", "70%"]}>
+        <TopButtons />
+        <Inputs />
+        {weather && (
+          <>
+            <TimeAndLocation />
+            <TempAndDetails />
+            <Forecast title="hourly" />
+            <Forecast title="daily" />
+          </>
+        )}
+        {!weather && (
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="xl"
+          />
+        )}
+      </VStack>
+    </>
   );
 }
 
 export default App;
-
-//11:50
-//005feeb72e0838e3972590465735efbb
-
-//City name:
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-
-//Mumbai
-// lat: 19.0144
-// lon: 72.8479
